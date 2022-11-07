efe6e949a3f4faf6f6821863dce156f164ff26ff
1Gt2 <117560596+1Gt2@users.noreply.github.com>
2022-11-07 19:12:50 +0800

    Add files via upload

--- 11_4_1.cpp
+++ 11_4_1.cpp
@@ -1,6 +1,27 @@
 #include<stdio.h>
+#include <stdlib.h>
 int main()
-{char str[]="Hello World!";
-puts(str);
-return 0;
+{
+    int n,i,j;
+    int b=0,y=0;
+    int *array;
+    printf("输入整数个数 n: ");
+    scanf("%d", &n);
+    array = (int *)malloc(n * sizeof(int));
+    for(int x=0;x<n;x++)
+    {
+        scanf("%d",&array[x]);
+    }
+
+    for(j=0;j<n;j++)
+    {
+        b=0;
+        for(i = j; i < n; i++)
+        {
+            b=b+array[i];
+            if(b>y)
+              y=b;
+        }
+    }
+    printf("最大子段和为%d",y);
 }
